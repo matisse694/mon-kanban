a9d81ede6eaf80702bfe066186f170bdf89457a7
Matisse NEAU <lubin.vezant@etu.univ-st-etienne.fr>
2026-04-30 11:29:07 +0200

    Test déploiement

--- mon-kanban/src/App.jsx
+++ mon-kanban/src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div style={{ fontFamily: 'sans-serif', padding: '2rem', textAlign: 'center' }}>
-      <h1>🚀 Mon premier déploiement</h1>
+      <h1>🚀 Déploiement automatique</h1>
       <p>Projet réalisé par <strong>Matisse NEAU</strong></p>
       <p>Future application : gestion de tâches Kanban</p>
     </div>
